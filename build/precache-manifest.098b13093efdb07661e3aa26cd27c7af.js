8d63c925a8bf16c3553e5c86c292d83e5b620e33
AdeodatusAbdul <adeodatus.niyokwiringirwa@harakameds.com>
2022-11-10 19:55:11 +0200

    make it better

---
+++ build/precache-manifest.098b13093efdb07661e3aa26cd27c7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9873cc302d538adb099",
+    "url": "/static/css/main.401c7261.chunk.css"
+  },
+  {
+    "revision": "b9873cc302d538adb099",
+    "url": "/static/js/main.b9873cc3.chunk.js"
+  },
+  {
+    "revision": "debf383e57d37a4519fb",
+    "url": "/static/js/1.debf383e.chunk.js"
+  },
+  {
+    "revision": "0a57254ab5744c88a0ae",
+    "url": "/static/js/2.0a57254a.chunk.js"
+  },
+  {
+    "revision": "7b114e57b77f7e4ad8e1",
+    "url": "/static/js/runtime~main.7b114e57.js"
+  },
+  {
+    "revision": "137871705bad0e91e345377e57f5468c",
+    "url": "/index.html"
+  }
+];
